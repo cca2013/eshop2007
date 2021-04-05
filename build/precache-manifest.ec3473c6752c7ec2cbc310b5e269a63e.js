56d25bbe120caa0f74fed8b3b33af1236b90e373
unknown <skafosbiz@gmail.com>
2021-04-05 17:58:14 +0300

    make it better

---
+++ build/precache-manifest.ec3473c6752c7ec2cbc310b5e269a63e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4bcfa8445d0f63b106006d6a71b3362",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef31baea13f5c684d407",
+    "url": "/static/css/main.acf0792f.chunk.css"
+  },
+  {
+    "revision": "ff40a5551ff8c81c0c0c",
+    "url": "/static/js/2.11690a2e.chunk.js"
+  },
+  {
+    "revision": "f239cc04571ce07ee5e6657051470c75",
+    "url": "/static/js/2.11690a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef31baea13f5c684d407",
+    "url": "/static/js/main.388aaf1a.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "1a585610b61d0a7b39014b233c266895",
+    "url": "/static/media/logo.1a585610.png"
+  }
+]);
